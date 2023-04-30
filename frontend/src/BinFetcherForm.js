901f62bbf24b330a9b71658380b7e449b68616ac
Oli Lane <olil4ne@gmail.com>
2023-04-30 08:31:59 +0100

    Building blocks for a frontend

---
+++ frontend/src/BinFetcherForm.js
@@ -0,0 +1,9 @@
+
+export function BinFetcherForm() {
+    return (
+        <div className="bin-form">
+            <label>Postcode: <input type='text'></input></label>
+            <button>Bin Me</button>
+        </div>
+    );
+}

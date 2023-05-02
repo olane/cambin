816fc3f53f8c90cebc58609fbd19e87c84a525a0
Oli Lane <olil4ne@gmail.com>
2023-05-02 14:33:27 +0100

    Improve date handling

--- frontend/src/BinResult.js
+++ frontend/src/BinResult.js
@@ -1,8 +1,43 @@
+function isToday (date) { 
+    const tomorrow = new Date();
+    return isSameDate(date, tomorrow);
+}
+
+function isTomorrow (date) { 
+    const tomorrow = new Date();
+    tomorrow.setDate(tomorrow.getDate() + 1);
+    return isSameDate(date, tomorrow);
+}
+
+function isSameDate(date1, date2) {
+    return date1.getDate() === date2.getDate() &&
+        date1.getMonth() === date2.getMonth() &&
+        date1.getFullYear() === date2.getFullYear();
+}
 
 function renderSingleCollection(collection) {
     const roundTypesString = collection.roundTypes.join(" and ");
 
-    return (<p>{roundTypesString} collection on {collection.date}</p>)
+    const date = new Date(collection.date);
+
+    let dateString = date.toLocaleDateString(
+        'en-gb',
+        {
+            weekday: "long",
+            month: 'long',
+            day: 'numeric',
+        }
+    );
+    
+    if(isToday(date)) {
+        dateString += " (today!)";
+    }
+    
+    if(isTomorrow(date)) {
+        dateString += " (tomorrow!)";
+    }
+
+    return (<p>{roundTypesString} collection on {dateString}</p>)
 }
 
 export function BinResult({result, error}) {
@@ -10,11 +45,12 @@ export function BinResult({result, error}) {
         return (<div>oh no</div>);
     }
 
-    const collections = result.collections.map(renderSingleCollection);
+    const collectionsRendered = result.collections.map(renderSingleCollection);
 
     return (
         <div className="bin-result">
-            {collections}
+            <h2>Your upcoming collections:</h2>
+            {collectionsRendered}
         </div>
     );
 }

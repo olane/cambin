b30e06218c93a14a22203eb5b3fd448771cd3385
Oli Lane <olil4ne@gmail.com>
2023-05-02 12:59:19 +0100

    render basic bin collection list

--- frontend/src/BinResult.js
+++ frontend/src/BinResult.js
@@ -1,12 +1,20 @@
 
+function renderSingleCollection(collection) {
+    const roundTypesString = collection.roundTypes.join(" and ");
+
+    return (<p>{roundTypesString} collection on {collection.date}</p>)
+}
+
 export function BinResult({result, error}) {
     if(error) {
         return (<div>oh no</div>);
     }
 
+    const collections = result.collections.map(renderSingleCollection);
+
     return (
         <div className="bin-result">
-            {result.collections[0].roundTypes[0]}
+            {collections}
         </div>
     );
 }

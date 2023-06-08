7c2ee67380c3896e400f301a431dca7bffb6bd77
Oli Lane <olil4ne@gmail.com>
2023-06-08 11:47:03 +0100

    Move calendar styling to its own file and modify the default style instead of overriding it

--- frontend/src/components/UpcomingCollectionsCalendar.tsx
+++ frontend/src/components/UpcomingCollectionsCalendar.tsx
@@ -1,6 +1,6 @@
 import React, { useState, FC } from 'react';
 import Calendar from 'react-calendar';
-import 'react-calendar/dist/Calendar.css';
+import '../styles/calendar.css';
 import { TileClassNameFunc, Value } from 'react-calendar/dist/cjs/shared/types';
 import { BinSchedule, RoundType } from '../model/BinTypes';
 import { isSameDate } from '../utils/dateUtils';
